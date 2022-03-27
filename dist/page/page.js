6f8d38ab97c09e317a4996e1c1a361ee7ae7aa5e
JeongYounHan <51sarang@hanmail.net>
2022-03-27 10:21:42 +0900

    dialog 컴포넌트 추가

--- dist/page/page.js
+++ dist/page/page.js
@@ -1,3 +1,4 @@
+import { InputDialog } from "./../components/page/dialog.js";
 import { Card } from "./../components/page/card.js";
 import { BaseComponent } from "./../components/component.js";
 import { ListImpl } from "../components/page/list.js";
@@ -6,5 +7,16 @@ export class PageComponent extends BaseComponent {
         super(`<article class="page"></article>`);
         const list = new ListImpl(Card);
         list.attachTo(this.element, "beforeend");
+        const imageBtn = document.querySelector("#new-image");
+        imageBtn.addEventListener("click", () => {
+            const dialog = new InputDialog();
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnSubmitListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.attachTo(document.body);
+        });
     }
 }
--- src/components/page/card.ts
+++ src/components/page/card.ts
@@ -1,5 +1,6 @@
 import { BaseComponent, Component } from "./../component.js";
 
+// 안에 다른 컴포넌트 넣을 수 있었으면 할때 사용
 export interface Composable {
   addChild(child: Component): void;
 }
